0e347d06971d0c7cfa5054cb8b3a4e5d0d4e4182
Court Hans <cornta@yahoo.com>
2020-06-02 16:41:29 -0700

    add JS question  + html and css cleanup

--- js/app.js
+++ js/app.js
@@ -15,22 +15,37 @@ Does Courtney know how to write wicked good code?
 */
 
 
-  // var userName = prompt('What\'s your preferred name?');
+  var userName = prompt('What\'s your preferred name?');
 
-  // var today = new Date();
-  // var hourNow = today.getHours();
-  // var greeting;
+  var today = new Date();
+  var hourNow = today.getHours();
+  var greeting;
 
-  // if (hourNow > 18) {
-  //   greeting = 'Buonasera, ';
-  // } else if (hourNow > 12) {
-  //   greeting = 'Buona pomeriggio, ';    
-  // } else if (hourNow > 0) {
-  //   greeting = 'Buongiorno, ';
-  // } else {
-  //   greeting = 'Bienvenuto, ';
-  // }
+  if (hourNow > 18) {
+    greeting = 'Buonasera, ';
+  } else if (hourNow > 12) {
+    greeting = 'Buon pomeriggio, ';    
+  } else if (hourNow > 0) {
+    greeting = 'Buongiorno, ';
+  } else {
+    greeting = 'Bienvenuto, ';
+  }
   
-  // document.write('<h4>' + greeting + userName + '! </h4>');
+  alert('Welcome to my page, ' + userName + ', let\'s see how much you already know about me...');
+  document.write('<h2>' + greeting + userName + '! </h2>');
+
+  var guessCourtneyAge = prompt('If you HAD to guess, how old (or young!) do you think Courtney is?')
+  var guessCourtneyAgeAsANumber = parseInt(guessCourtneyAge);
+
+  if (guessCourtneyAgeAsANumber > 39 && guessCourtneyAgeAsANumber < 49) {
+    alert('You\'re pretty close, we\'ll call it good!');
+  } else if(guessCourtneyAgeAsANumber <= 39) {
+    alert('Aren\'t you the flatterer?');
+  } else if (guessCourtneyAgeAsANumber >= 49){
+    alert('Hmm, you think so? Guess I need a better skin care regimen. Noted.');
+  } else {
+    alert('Age is just a number. So enter one, please.')
+  }
+  
 
 
