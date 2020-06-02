c0d5404f43269d8157759e486e4b2bcfa8aa5c36
Court Hans <cornta@yahoo.com>
2020-06-02 16:14:04 -0700

    css styling and first JS app

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# about-me-
+# about-me
 201 Lab 2 assignment
+
+This file runs my Lab 2 project.  
+MIT license
+
--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
 'use strict';
 
-var name = ('courtney');
-console.log(name);
+/*
+About Courtney
+
+Ask user his/her name through prompt()
+Display username back through customized greeting
+Display user name back in final message to user
+
+How old is Courtney?
+Does Courtney have an identical twin?
+Does Courtney love bad jokes?
+Does Courtney have kids?
+Does Courtney know how to write wicked good code?
+*/
+
+
+  // var userName = prompt('What\'s your preferred name?');
+
+  // var today = new Date();
+  // var hourNow = today.getHours();
+  // var greeting;
+
+  // if (hourNow > 18) {
+  //   greeting = 'Buonasera, ';
+  // } else if (hourNow > 12) {
+  //   greeting = 'Buona pomeriggio, ';    
+  // } else if (hourNow > 0) {
+  //   greeting = 'Buongiorno, ';
+  // } else {
+  //   greeting = 'Bienvenuto, ';
+  // }
+  
+  // document.write('<h4>' + greeting + userName + '! </h4>');
+
+
