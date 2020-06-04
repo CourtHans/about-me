cf0b07ca1917769be9b779a5976be1eb633b1054
Court Hans <cornta@yahoo.com>
2020-06-03 22:21:43 -0700

    added final tally and changed question 4 from kids to Kilamanjaro

--- js/app.js
+++ js/app.js
@@ -16,138 +16,160 @@ Keep track of correct answers, at the end, tell them how many (out of 7) they go
 */
 
 var userName = prompt('What\'s your preferred name?');
+var finalScore; //for final tally
 
-// // ============for fun greeting at the bottom of the page============
-// var today = new Date();
-// var hourNow = today.getHours();
-// var greeting;
+// ========or fun greeting at the bottom of the page=========
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
 
-// if (hourNow > 18) {
-//   greeting = 'Buonasera, ';
-// } else if (hourNow > 12) {
-//   greeting = 'Buon pomeriggio, ';
-// } else if (hourNow > 0) {
-//   greeting = 'Buongiorno, ';
-// } else {
-//   greeting = 'Bienvenuto, ';
-// }
-// //============welcome to the page============
-// alert('Welcome to my page, ' + userName + ', let\'s see how much you already know about Courtney...');
-// document.write('<h2>' + greeting + userName + '! </h2>');
+if (hourNow > 18) {
+  greeting = 'Buonasera, ';
+} else if (hourNow > 12) {
+  greeting = 'Buon pomeriggio, ';
+} else if (hourNow > 0) {
+  greeting = 'Buongiorno, ';
+} else {
+  greeting = 'Bienvenuto, ';
+}
+//====================welcome to the page===================
+alert('Welcome to my page, ' + userName + ', let\'s see how much you already know about Courtney...');
+document.write('<h2>' + greeting + userName + '! </h2>');
 
-// //============question 1============
-// var twinYN = prompt('Does Courtney have a twin? (yes or no)').toLowerCase();
+//========================question 1========================
+var twinYN = prompt('Does Courtney have a twin? (yes or no)').toLowerCase();
 
-// if (twinYN === 'yes' || twinYN === 'y') {
-//   // console.log ('twin answer: correct');
-//   alert('You\'re right, she IS a twin!');
-// } else if (twinYN === 'no' || twinYN === 'n') {
-//   // console.log('twin answer: incorrect');
-//   alert('Wrong, she IS a twin.');
-// } else {
-//   // console.log('twin answer: invalid answer');
-//   alert('This is a y or n question. For your information, she IS a twin.');
-// }
+if (twinYN === 'yes' || twinYN === 'y') {
+  // console.log ('twin answer: correct');
+  alert('You\'re right, she IS a twin!');
+  var q1 = 1;
+} else if (twinYN === 'no' || twinYN === 'n') {
+  // console.log('twin answer: incorrect');
+  alert('Wrong, she IS a twin.');
+  q1 = 0;
+} else {
+  // console.log('twin answer: invalid answer');
+  alert('This is a y or n question. For your information, she IS a twin.');
+  q1 = 0;
+}
 
-// //============question 2============
-// var twinType= prompt('Now that you know, tell me..identical or fraternal twin?').toLowerCase();
+//========================question 2========================
+var twinType= prompt('Now that you know, tell me...identical or fraternal twin?').toLowerCase();
 
-// if (twinType === 'identical') {
-//   // console.log ('twin type: correct');
-//   alert('Correct! Courtney\'s an identical twin (also known as monozygous).');
-// } else if (twinType === 'fraternal') {
-//   // console.log('twin answer: incorrect');
-//   alert('Incorrect. Courtney is an IDENTICAL twin. Fun fact: fraternal twins are known as "dizygous twins."');
-// } else {
-//   // console.log('twin type: invalid answer');
-//   alert('Double check your spelling next time, whatever that was, it wasn\'t right. Courtney is an identical twin.');
-// }
+if (twinType === 'identical') {
+  // console.log ('twin type: correct');
+  alert('Correct! Courtney\'s an identical twin (also known as monozygous).');
+  var q2 = 1;
+} else if (twinType === 'fraternal') {
+  // console.log('twin answer: incorrect');
+  alert('Incorrect. Courtney is an IDENTICAL twin. Fun fact: fraternal twins are known as "dizygous twins."');
+  q2 = 0;
+} else {
+  // console.log('twin type: invalid answer');
+  alert('Double check your spelling next time, whatever that was, it wasn\'t right. Courtney is an identical twin.');
+  q2 = 0;
+}
 
-// //============question 3============
-// var jokeYN= prompt('Does she like bad jokes? (yes or no)').toLowerCase();
+//========================question 3========================
+var jokeYN= prompt('Does she like bad jokes? (yes or no)').toLowerCase();
 
-// if (jokeYN === 'yes' || jokeYN === 'y') {
-//   // console.log ('bad jokes: correct');
-//   alert('Like them? More like LOVES them!');
-// } else if (jokeYN === 'no' || jokeYN === 'n') {
-//   // console.log('bad jokes: incorrect');
-//   alert('For better or for worse, I\'m embarassed to say she LOVES them.');
-// } else {
-//   // console.log('bad jokes: invalid answer');
-//   alert('Invalid input - you were supposed to write yes or no. As for bad jokes? She LOVES \'em.');
-// }
+if (jokeYN === 'yes' || jokeYN === 'y') {
+  // console.log ('bad jokes: correct');
+  alert('Like them? More like LOVES them!');
+  var q3 = 1;
+} else if (jokeYN === 'no' || jokeYN === 'n') {
+  // console.log('bad jokes: incorrect');
+  alert('For better or for worse, I\'m embarassed to say she LOVES them.');
+  q3 = 0;
+} else {
+  // console.log('bad jokes: invalid answer');
+  alert('Invalid input - you were supposed to write yes or no. As for bad jokes? She LOVES \'em.');
+  q3 = 0;
+}
 
-// //============question 4============
-// var kidsYN= prompt('Does Courtney have kids (yes or no)?').toLowerCase();
+//========================question 4========================
+var summitYN= prompt('Has Courtney summitted Mt. Kilamanjaro? (yes or no)?').toLowerCase();
 
-// if (kidsYN === 'yes' || kidsYN === 'y') {
-//   // console.log ('kids: correct');
-//   alert('Indeed she does! She has a 12-year-old daugher and a 9-year-old son.');
-// } else if (kidsYN === 'no' || kidsYN === 'n') {
-//   // console.log('kids: incorrect');
-//   alert('She has two kids, in fact. A 9-year-old son and a 12-year-old daughter.');
-// } else {
-//   // console.log('kids: invalid answer');
-//   alert('It was a simple yes or no. If you\'re curious, she has two.');
-// }
+if (summitYN === 'no' || summitYN === 'n') {
+  // console.log ('kids: correct');
+  alert('You\'re right, she hasn\'t climbed Africa\'s tallest peak yet - but she hopes to soon!');
+  var q4 = 1;
+} else if (summitYN === 'yes' || summitYN === 'y') {
+  // console.log('kids: incorrect');
+  alert('Wrong. (But she wishes this were true.)');
+  q4 = 0;
+} else {
+  // console.log('kids: invalid answer');
+  alert('It was a simple yes or no. If you\'re curious, she hasn\'t yet summited Mt. Kilamajaro - but she hopes to one day!');
+  q4 = 0;
+}
 
-// //============question 5============
-// var codeYN= prompt('Last one: can Courtney code? (yes or no)').toLowerCase();
+//========================question 5========================
+var codeYN= prompt('Next up: can Courtney code? (yes or no)').toLowerCase();
 
-// if (codeYN === 'yes' || codeYN === 'y') {
-//   // console.log ('but can she code?: correct');
-//   alert('DANG STRAIGHT SHE CAN - like a MOTHER! ' + userName + ', you must know her well!');
-// } else if (codeYN === 'no' || codeYN === 'n') {
-//   // console.log('but can she code?: incorrect');
-//   alert(userName + ', really? OF COURSE SHE CAN!');
-// } else {
-//   // console.log('but can she code?: invalid answer');
-//   alert('Was a "y" or "n" too difficult? C\'mon, ' + userName + ', you\'re better than that! For the record, Courtney can code LIKE A MOTHER!');
-// }
+if (codeYN === 'yes' || codeYN === 'y') {
+  // console.log ('but can she code?: correct');
+  alert('DANG STRAIGHT SHE CAN - like a MOTHER! ' + userName + ', you must know her well!');
+  var q5 = 1;
+} else if (codeYN === 'no' || codeYN === 'n') {
+  // console.log('but can she code?: incorrect');
+  alert(userName + ', really? OF COURSE SHE CAN!');
+  q5 = 0;
+} else {
+  // console.log('but can she code?: invalid answer');
+  alert('Was a "y" or "n" too difficult? C\'mon, ' + userName + ', you\'re better than that! For the record, Courtney can code LIKE A MOTHER!');
+  q5 = 0;
+}
 
-// //============question 6============
-// //special thanks to Matt Herriges and Richard Lee for help here
-// var triesShowerDays = 0;
-// var guessShowerDays;
-// var answerShowerText = 'Courtney actually went 28 days without a shower while on a backpacking expedition through the North Rim of the Grand Canyon. Gross, I know.';
+//========================question 6========================
+//special thanks to Matt Herriges and Richard Lee for help here
+var triesShowerDays = 0;
+var guessShowerDays;
+var answerShowerText = 'Courtney actually went 28 days without a shower while on a backpacking expedition through the North Rim of the Grand Canyon. Gross, I know.';
 
-// while (triesShowerDays < 4 && guessShowerDaysAsANumber !== 28){
-//   var attemptsLeft = 4 - triesShowerDays;
-//   guessShowerDays = prompt('What\'s the record number of days Courtney\'s ever gone without a shower? Attempts left: ' +attemptsLeft);
-//   var guessShowerDaysAsANumber = parseInt (guessShowerDays);
-//   if (guessShowerDaysAsANumber === 28){
-//     alert('WOW - you got it right, ' + userName + '! Yes, ' + answerShowerText);
-//   } else if (guessShowerDaysAsANumber >= 29 && guessShowerDaysAsANumber < 34) {
-//     alert('You\'re just a little high.');
-//   } else if (guessShowerDaysAsANumber >= 34) {
-//     alert('Ew, no. Not THAT long. You\'ve guessed way too high');
-//   } else if (guessShowerDaysAsANumber < 28 && guessShowerDaysAsANumber > 21){
-//     alert('You\'re just a tad low.');
-//   } else if (guessShowerDaysAsANumber <= 21){
-//     alert('Nuh uh. It was WAY longer than that.');
-//   } else {
-//     alert('C\'mon! Guess a number!');
-//   }
-//   triesShowerDays++;
-//   console.log(triesShowerDays);
-// }
-// if (triesShowerDays === 4 && guessShowerDaysAsANumber !== 28){
-//   alert('I\'m sorry, ' + userName + ', you\'ve exhausted your guess. ' + answerShowerText);
-// }
+while (triesShowerDays < 4 && guessShowerDaysAsANumber !== 28){
+  var attemptsLeft = 4 - triesShowerDays;
+  guessShowerDays = prompt('What\'s the record number of days Courtney\'s ever gone without a shower? (attempts left: ' +attemptsLeft + ')');
+  var guessShowerDaysAsANumber = parseInt (guessShowerDays);
+  if (guessShowerDaysAsANumber === 28){
+    alert('WOW - you got it right, ' + userName + '! Yes, ' + answerShowerText);
+    var q6 = 1;
+  } else if (guessShowerDaysAsANumber >= 29 && guessShowerDaysAsANumber < 34) {
+    alert('You\'re just a little high.');
+  } else if (guessShowerDaysAsANumber >= 34) {
+    alert('Ew, no. Not THAT long. You\'ve guessed way too high');
+  } else if (guessShowerDaysAsANumber < 28 && guessShowerDaysAsANumber > 21){
+    alert('You\'re just a tad low.');
+  } else if (guessShowerDaysAsANumber <= 21){
+    alert('Nuh uh. It was WAY longer than that.');
+  } else {
+    alert('C\'mon! Guess a number!');
+  }
+  triesShowerDays++;
+  console.log(triesShowerDays);
+}
+if (triesShowerDays === 4 && guessShowerDaysAsANumber !== 28){
+  alert('I\'m sorry, ' + userName + ', you\'ve exhausted your guess. ' + answerShowerText);
+  q6 = 0;
+}
 
+//========================question 7========================
 var countryArray = ['australia', 'austria', 'belgium', 'canada', 'chile', 'czech republic', 'czechrepublic', 'france', 'germany', 'hungary', 'iceland', 'indonesia', 'ireland', 'italy', 'luxembourg', 'mexico', 'netherlands', 'new zealand', 'newzealand', 'norway', 'peru', 'spain', 'united kingdom', 'unitedkingdom', 'vietnam'];
 var guessAttempts = 0;
 
 for (var guess = 0; guess < 6; guess++){
   var countryAttemptsLeft = 6 - guessAttempts;
-  var countryAnswer = prompt('Guess a country (outside the U.S.) that Courtney\'s been to? Attempts left: ' + countryAttemptsLeft).toLowerCase();
+  var countryAnswer = prompt('Guess a country (outside the U.S.) that Courtney\'s been to? (attempts left: ' + countryAttemptsLeft + ')').toLowerCase();
 
   if (countryAnswer === countryArray[0] || countryAnswer === countryArray[1] || countryAnswer === countryArray [2] || countryAnswer === countryArray[3] || countryAnswer === countryArray[4] || countryAnswer === countryArray[5] || countryAnswer === countryArray[6] || countryAnswer === countryArray[7] || countryAnswer === countryArray[8] || countryAnswer === countryArray[9] || countryAnswer === countryArray[10] || countryAnswer === countryArray[11] || countryAnswer === countryArray[12] || countryAnswer === countryArray[13] || countryAnswer === countryArray[14] || countryAnswer === countryArray[15] || countryAnswer === countryArray[16] || countryAnswer === countryArray[17] || countryAnswer === countryArray[18] || countryAnswer === countryArray[19] || countryAnswer === countryArray[20] || countryAnswer === countryArray[21]){
-    alert('Yes! That is one of the 22 countries Courtney has been too! Good job, ' + userName + '!');
+    alert('Yes! That is one of the 22 countries Courtney has been too! In case you\'re curious, here\'s the full list: Australia, Austria, Belgium, Canada, Czech Republic, France, Germany, Hungary, Iceland, Indonesia, Ireland, Italy, Luxembourg, Mexico, Netherlands, New Zealand, Norway, Spain, United Kingdom, and Vietnam.');
+    var q7 = 1;
     break;
-  } else if (countryAnswer !== countryArray[0] || countryAnswer !== countryArray[1] || countryAnswer !== countryArray [2] || countryAnswer !== countryArray[3] || countryAnswer !== countryArray[4] || countryAnswer !== countryArray[5] || countryAnswer !== countryArray[6] || countryAnswer !== countryArray[7] || countryAnswer !== countryArray[8] || countryAnswer !== countryArray[9] || countryAnswer !== countryArray[10] || countryAnswer !== countryArray[11] || countryAnswer !== countryArray[12] || countryAnswer !== countryArray[13] || countryAnswer !== countryArray[14] || countryAnswer !== countryArray[15] || countryAnswer !== countryArray[16] || countryAnswer !== countryArray[17] || countryAnswer !== countryArray[18] || countryAnswer !== countryArray[19] || countryAnswer !== countryArray[20] || countryAnswer !== countryArray[21]){
-    alert('Nope, she hasn\'t been there... yet.');
+  } else {
+    alert('Nope, she hasn\'t been there...yet.');
+    q7 = 0;
   }
+
   guessAttempts++;
   console.log('country guess attempts,' + guessAttempts);
 }
@@ -155,4 +177,12 @@ if (guessAttempts === 6){
   alert('Wow. Courtney\'s been to 22 countries, and you couldn\'t guess one? Here\'s the full list: Australia, Austria, Belgium, Canada, Czech Republic, France, Germany, Hungary, Iceland, Indonesia, Ireland, Italy, Luxembourg, Mexico, Netherlands, New Zealand, Norway, Spain, United Kingdom, and Vietnam.');
 }
 
+finalScore = q1 + q2 + q3 + q4 + q5 + q6 + q7;
 
+if (finalScore >= 6){
+  alert('Wow ' + userName + ', you scored ' + finalScore + ' out of 7 - GREAT job!');
+} else if (finalScore < 6 && finalScore >= 4){
+  alert('You scored ' + finalScore + ' out of 7. Mediocre job, ' + userName + '.');
+} else {
+  alert('Hate to break it to you, ' + userName + ', but you got ' + finalScore + ' out of 7 right. Ah well, growth mindset, right?');
+}
